b8511ac84f70ef3cebd64fbf620af54035a752c2
cobianic <vitrage@protonmail.com>
2023-10-10 23:35:04 +0300

    now meal data fetches from the database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,27 +1,72 @@
-import React, {useContext} from 'react';
+import React, {useEffect, useState} from 'react';
 import './AvailableMeals.sass';
 import MealItem from './MealItem';
 import Card from '../UI/Card';
-import MealsContext from "../store/meals-context";
 
 const AvailableMeals = () => {
-  const { meals } = useContext(MealsContext);
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setIsLoading(true)
+    setError(null)
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          'https://react-fetch-movies-ad76e-default-rtdb.europe-west1.firebasedatabase.app/food-order-app.json'
+        )
+        if (!response.ok) {
+          throw new Error('An error occurred. Status: ' + response.status)
+        }
+        const data = await response.json();
+        const loadedMeals = []
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price
+          })
+        }
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message)
+      }
+      setIsLoading(false)
+    }
+    fetchMeals()
+  }, [setIsLoading, setError])
+
+  let content = <p>Found no movies</p>
+
+  if (meals.length > 0) {
+    content = <Card>
+      <ul>
+        {meals.map((meal) => (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        ))}
+      </ul>
+    </Card>
+  }
+
+  if (error) {
+    content = <Card>{error}</Card>
+  }
+
+  if (isLoading) {
+    content = <Card>Loading...</Card>
+  }
 
   return (
     <section className='meals'>
-      <Card>
-          <ul>
-            {meals.map((meal) => (
-              <MealItem
-                key={meal.id}
-                id={meal.id}
-                name={meal.name}
-                description={meal.description}
-                price={meal.price}
-              />
-            ))}
-          </ul>
-      </Card>
+      {content}
     </section>
   )
 }
